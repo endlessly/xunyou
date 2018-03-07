005724295440b56e2d9c61164b1fe8f1c7be9d4b
ck <109670133@qq.com>
2018-03-08 00:22:39 +0800

    pom

---
+++ src/main/java/com/xunyou/action/UserAction.java
@@ -0,0 +1,11 @@
+package com.xunyou.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserAction {
+    @RequestMapping("/index")
+    public void Index() {
+    }
+}
