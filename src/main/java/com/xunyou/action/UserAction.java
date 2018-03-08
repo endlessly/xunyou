f6fccd47eaef8da66ef3ad4b89aee31d88006f1b
ck <109670133@qq.com>
2018-03-08 22:08:41 +0800

    pom

--- src/main/java/com/xunyou/action/UserAction.java
+++ src/main/java/com/xunyou/action/UserAction.java
@@ -2,10 +2,13 @@ package com.xunyou.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class UserAction {
-    @RequestMapping("/index")
-    public void Index() {
+    @RequestMapping("/indexs")
+    public String Index() {
+        return "fsdf";
     }
 }
