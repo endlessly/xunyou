08243ea5f39cf3a4a80d0ca6a685d5efc77cb0b1
huruisc <450326591@qq.com>
2018-03-10 12:37:28 +0800

    添加登录功能

--- src/main/java/com/xunyou/action/UserAction.java
+++ src/main/java/com/xunyou/action/UserAction.java
@@ -1,20 +1,24 @@
 package com.xunyou.action;
 
+import com.alibaba.fastjson.JSON;
 import com.xunyou.model.UserEntity;
+import com.xunyou.model.UserInfoEntity;
 import com.xunyou.service.UserService;
+import com.xunyou.utils.RequestUtil;
+import com.xunyou.utils.ResultMsgDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @ResponseBody
@@ -43,14 +47,21 @@ public class UserAction  {
 //        session.put("user",user);
        HttpSession session= request.getSession();
        session.setAttribute("user",user);
-        return "success";
+        return  "success";
     }
 
     @RequestMapping (value = "cklogin")
     public String ckLogin(HttpServletRequest request, HttpServletResponse response) {
-          UserEntity user=(UserEntity) request.getSession().getAttribute("user");
-          if(user==null)
-              return  "unlogin";
-            return "login";
+        UserEntity user=(UserEntity) request.getSession().getAttribute("user");
+        if(user==null)
+            return  "unlogin";
+        return "login";
     }
-}
+
+    @RequestMapping (value = "register")
+    public String register(@RequestBody HttpServletRequest request) {
+    System.out.println(request.getParameterNames());
+        return "";
+    }
+
+    }
